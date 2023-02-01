fbc3028fdc226805dd0bbe1a229315e51434f442
oretha francis <francis.oretha@selu.edu>
2023-02-01 12:25:09 -0600

    commit

--- SP23.P01.Tests.Web/DataContextTests.cs
+++ SP23.P01.Tests.Web/DataContextTests.cs
@@ -108,4 +108,5 @@ public class DataContextTests
             return null;
         }
     }
+
 }
--- SP23.P01.Web/Program.cs
+++ SP23.P01.Web/Program.cs
@@ -1,6 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DataContext")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -8,6 +10,13 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
